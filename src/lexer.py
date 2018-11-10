cebc62d731cd845563b7bdd8303e7d7dee27cec7
Vera Releyn <vreleyn@gmail.com>
2018-11-10 16:15:59 -0600

    Added support for lexing integers for Issue #1

--- src/lexer.py
+++ src/lexer.py
@@ -8,9 +8,26 @@ class Lexer:
     def __str__(self):
         return "Lexer with code: (" + self.code.__str__() + ")"
 
-    def get_token(self, index):
-        return None
+    def isdigit(self, char):
+        return char >= '0' and char <= '9'
 
+    def get_token(self, index):
+        char = self.get_char(self.code, index)
+        if char >= '0' and char <= '9':
+            return self.get_match_token(index, self.isdigit)
+        else:
+            return None
+        return token
+
+    def get_char(self, code, index):
+        return code[index] if len(code) > index else chr(0)
+
+    def get_match_token(self, index, fn):
+        length = 0
+        while(fn(self.get_char(self.code, index+length))):
+            length = length + 1
+        return Token(self.code[index:index+length], "file", 0, 0)
+               
     def get_tokens(self):
         return None
 
